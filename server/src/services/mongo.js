6ca76249450a9e4a5509663b6659529dcfbdb304
Myroslav Tkachenko <myroslav.tkachenko@gmail.com>
2021-09-04 18:21:45 +0300

    initial commit

---
+++ server/src/services/mongo.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
+
+async function mongoConnect() {
+  mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+}
+
+async function mongoDisconnect() {
+  await mongoose.disconnect();
+}
+
+module.exports = {
+  mongoConnect,
+  mongoDisconnect,
+};
